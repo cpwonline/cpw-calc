f92af8557cf5706f81c420d431a5a04376f6a71f
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2020-04-27 11:27:25 -0400

    feat: Add some members function implementation
    Some members of calc class.

--- src/calc.cpp
+++ src/calc.cpp
@@ -2,10 +2,56 @@
 
 calc::calc()
 {
-    //ctor
+    setResult(0.00);
+    setBuffer(0.00);
+    error = false;
 }
 
-calc::~calc()
+float calc::getResult()
 {
-    //dtor
+    return this->result;
+}
+float calc::getBuffer()
+{
+    return this->buffer;
+}
+float calc::getHistorial()
+{
+    return this->historial;
+}
+
+void calc::setResult(float r)
+{
+    this->result = r;
+}
+void calc::setBuffer(float b)
+{
+    this->buffer = b;
+}
+void calc::setHistorial(float h)
+{
+    this->historial = h;
+}
+
+float calc::sum(float a, float b)
+{
+    return a + b;
+}
+float calc::subtract(float a, float b)
+{
+    return a - b;
+}
+float calc::multiplicate(float a, float b)
+{
+    return a * b;
+}
+float calc::divide(float a, float b)
+{
+    if(b != 0)
+        return a / b;
+    else
+    {
+        error = true;
+        return 0.0;
+    }
 }
